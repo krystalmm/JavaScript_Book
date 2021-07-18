131866b1cac0b54fa9e0dd5861a8afedeb577b4e
krystalmm <taeyeon_aina@live.jp>
2021-07-18 16:18:05 +0900

    [add] paper.js

--- ch02/main.js
+++ ch02/main.js
@@ -1,1 +1,10 @@
-console.log('main.jsをロードした！');
+$(document).ready(function () {
+  'use strict';
+
+  paper.install(window);
+  paper.setup(document.getElementById('mainCanvas'));
+  paper.view.draw();
+
+  let c = Shape.Circle(200, 200, 50);
+  c.fillColor = 'green';
+});
