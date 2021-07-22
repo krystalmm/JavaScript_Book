928f76e2ca96f895f69d858b02b53a57a5ac92e9
krystalmm <taeyeon_aina@live.jp>
2021-07-22 12:38:00 +0900

    [Start] ch05

---
+++ ch05/main.js
@@ -0,0 +1,18 @@
+/**
+ * 5章 四季と演算子
+ */
+
+// インクリメントとデクリメント
+let x = 2;
+console.log(x++ + x++);
+console.log(++x + ++x);
+console.log(x++ + ++x);
+console.log(++x + x++);
+console.log(x);
+console.log("--------");
+let y = 10;
+console.log(y-- + y--);
+console.log(--y + --y);
+console.log(y-- + --y);
+console.log(--y + y--);
+console.log(y);
