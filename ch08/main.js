2017da945aaf1d04907f40eec9786736b01d62eb
krystalmm <taeyeon_aina@live.jp>
2021-08-03 22:06:34 +0900

    [update] ch08 main.js

--- ch08/main.js
+++ ch08/main.js
@@ -2,7 +2,7 @@
  * 8章 配列
  */
 
-// 先頭あるいは最後の要素に対する操作
+/** 先頭あるいは最後の要素に対する操作 */
 // pushとunshiftの戻り値は変更後の配列の長さ、popとshiftは削除された要素を戻す！
 let arr = ["b", "c", "d"];
 console.log(arr.push("e")); // 4 ← 現在の長さ（要素数）
@@ -13,3 +13,36 @@ console.log(arr.unshift("a")); // 4 ← 現在の長さ
 console.log(arr); // [ 'a', 'b', 'c', 'd' ]
 console.log(arr.shift()); // a
 console.log(arr); // [ 'b', 'c', 'd' ]
+
+
+/** 複数要素の追加 */
+let arr2 = [1, 2, 3];
+let arr3 = arr2.concat(4, 5, 6);
+console.log(arr2); // [ 1, 2, 3 ]
+console.log(arr3); // [ 1, 2, 3, 4, 5, 6 ]
+arr3 = arr2.concat([4, 5, 6]); // 配列を渡す
+console.log(arr3); // [ 1, 2, 3, 4, 5, 6 ]
+arr3 = arr2.concat([4, 5], 6);
+console.log(arr3); // [ 1, 2, 3, 4, 5, 6 ]
+arr3 = arr2.concat([4, 5], [6, 7]); // 引数は2つでいずれも配列
+console.log(arr3); // [ 1, 2, 3, 4, 5, 6, 7 ]
+arr3 = arr2.concat([4, [5, 6]]); // 引数は配列一つでその2番目の要素が配列
+console.log(arr3); // [ 1, 2, 3, 4, [ 5, 6 ] ]
+
+
+/** 途中の要素の削除や途中への要素の追加 */
+let arr4 = [1, 5, 7];
+let arr5 = arr4.splice(1, 0, 2, 3, 4); // arr4[1]から2, 3, 4が追加される
+console.log(arr4); // [ 1, 2, 3, 4, 5, 7 ]
+console.log(arr5); // [] ← 何も削除されていない
+arr5 = arr4.splice(5, 0, 6); // arr4[5]に6が追加されて、以降一つずつ後ろへ
+console.log(arr4); // [1, 2, 3, 4, 5, 6, 7]
+console.log(arr5); // [] ← 何も削除されていない
+arr5 = arr4.splice(1, 2); // arr4[1]から2個削除
+console.log(arr4); // [ 1, 4, 5, 6, 7 ]
+console.log(arr5); // [ 2, 3 ] ← 削除された要素
+arr5 = arr4.splice(2, 1, 'a', 'b'); // arr4[2]から1個削除して、'a'と'b'をそこに追加
+console.log(arr4); // [ 1, 4, 'a', 'b', 6, 7 ]
+console.log(arr5); // [5] ← 削除された要素
+
+
